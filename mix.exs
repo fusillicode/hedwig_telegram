b68aaa28aa6f12ca023a8efded35d2103da259b0
fusillicode <fusillicode@gmail.com>
2019-09-21 10:52:46 +0200

    version-0.1.0

--- mix.exs
+++ mix.exs
@@ -22,8 +22,9 @@ defmodule HedwigTelegram.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:hedwig, "~> 1.0"},
+      {:jason, "~> 1.0"},
+      {:tesla, "~> 1.2.1"}
     ]
   end
 end
