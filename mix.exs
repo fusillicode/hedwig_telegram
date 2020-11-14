ee9eb2485ec4c559f598d980b19432c8487ee0a0
gpanice <giovanni.panice@lastminute.com>
2020-11-14 21:30:03 +0100

    new release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HedwigTelegram.MixProject do
   def project do
     [
       app: :hedwig_telegram,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
